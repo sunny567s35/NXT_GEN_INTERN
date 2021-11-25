e1078dcaec78a36a32c9e00fb1caab6ef6e730a7
sunny567s35 <sunny567s35@gmail.com>
2021-11-25 16:02:52 +0530

    intial commit

---
+++ practise_algos/searching_1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner i= new Scanner(System.in);
+        int j=0;
+        System.out.println("Enter the size of the array : ");
+        
+        int size = i.nextInt();
+        System.out.println("Enter the key to be searched : ");
+        int key = i.nextInt();
+        System.out.println("Enter the array elements : ");
+        int[] arr = new int[size];
+        int count = 0;
+        boolean flag = false;
+        for(j=0;j<size;j++){
+             arr[j] = i.nextInt();
+            
+        }
+        for(j=0;j<size;j++){
+            
+            if( arr[j] == key){
+                System.out.println("Element found at "+j+" index in the array !");
+                flag = true;
+                break;
+            }
+            else{count++;}
+            
+        }if(count==size-1 && flag == false){System.out.println("Element not found in the array !");}
+            
+       
+    }
+    
+}
