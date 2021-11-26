6da86c6a2a1decbe2b46cd98d1414200dc6a4cb9
sunny567s35 <sunny567s35@gmail.com>
2021-11-26 19:56:47 +0530

    intial commit

---
+++ practise_algos/duplicate_frequency.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class duplicate_frequency {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int count =0;
+        int freq = 1;
+        System.out.println("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        System.out.println("Enter the elements of the array : ");
+        int[] ar = new int[size];
+        //intializing the array with elements
+        for(int a=0;a<size;a++){ar[a] = sc.nextInt();}
+
+        for(int i =0 ; i<size;i++){
+            count =1;
+            
+            for(int j =i+1 ;j < size && ar[j] != 10000; j++ ){
+                
+                    if(ar[i] == ar[j]){
+                        ar[j] = 10000;
+                        freq++;
+
+                    }
+                
+            }
+            if(freq >1){
+                System.out.println(freq +" ");
+                freq =1 ;
+            }
+            else if(ar[i] == 10000){System.out.println("d ");freq =1;
+        }
+        else{System.out.println(count+" ");freq =1 ;
+    }
+        }
+
+    }
+    
+}
